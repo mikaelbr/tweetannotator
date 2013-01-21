d807ce0cca295e5566a618b7ccffe635d4042627
Mikael Brevik <mikaelbre@gmail.com>
2013-01-21 15:23:33 +0100

    Added config file with env variables

---
+++ config/default.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "Twitter": {
+    "consumer_key": process.env.TWEET_ANNO_consumer_key,
+    "consumer_secret": process.env.TWEET_ANNO_consumer_secret,
+    "access_token_key": process.env.TWEET_ANNO_access_token_key,
+    "access_token_secret": process.env.TWEET_ANNO_access_token_secret
+  }
+}
