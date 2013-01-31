bb4516997663bec89013d5a14be02627eb34a450
Mikael Brevik <mikaelb@mikaelb.net>
2013-01-31 09:26:45 +0100

    Added MongoDB as storage for double fail safe

--- config/default.js
+++ config/default.js
@@ -4,5 +4,8 @@ module.exports = {
     "consumer_secret": process.env.TWEET_ANNO_consumer_secret,
     "access_token_key": process.env.TWEET_ANNO_access_token_key,
     "access_token_secret": process.env.TWEET_ANNO_access_token_secret
+  },
+  "MongoJS": {
+    "connect_url": process.env.MONGOHQ_URL
   }
-}
+};
